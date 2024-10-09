3b9ee2e53efdf9a9b964b902957664a520ab640e
abdul-zabbar04 <a4zabbar@gmail.com>
2024-10-09 06:31:19 +0600

    C:/Program Files/Git/ remove in media

--- house_rent/settings.py
+++ house_rent/settings.py
@@ -175,7 +175,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
-MEDIA_URL ='/media/'
+MEDIA_URL ='media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
